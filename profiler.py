33604b84c66ced83e9108b44acbd1bf0853879c7
hvolos01 <hvolos01@node0.hvolos01-110325.ucy-cs499-dc-pg0.wisc.cloudlab.us>
2021-11-10 12:05:51 -0600

    get power state usage and time

--- profiler.py
+++ profiler.py
@@ -3,6 +3,7 @@ import xmlrpc.client
 import argparse
 import functools
 import logging
+import os
 import sys
 import socket
 import time
@@ -14,28 +15,37 @@ class Profiling:
     def __init__(self):
         self.terminate = threading.Condition()
         self.is_active = False
+        self.events = self.get_perf_power_events()
+        self.timeseries = {}
 
-    def perf_power_events(self):
+    def get_perf_power_events(self):
+        events = []
         result = subprocess.run(['perf', 'list'], stdout=subprocess.PIPE)
         for l in result.stdout.decode('utf-8').splitlines():
-            m = re.match("\s*power/energy-(.*)/\s*\[Kernel PMU event]", l)
+            l = l.lstrip()
+            m = re.match("(power/energy-.*/)\s*\[Kernel PMU event]", l)
             if m:
-                print(m.group(1))
-            
+                events.append(m.group(1))
+        return events
 
-    def perf_stat_power(self):
-        #result = subprocess.run(['perf', 'stat'], stdout=subprocess.PIPE)
-        print(result.stdout.decode('utf-8'))
+    def sample_perf_stat_power(self):
+        events_str = ','.join(self.events)
+        cmd = ['sudo', 'perf', 'stat', '-a', '-e', events_str, 'sleep', '1']
+        result = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        out = result.stdout.decode('utf-8').splitlines() + result.stderr.decode('utf-8').splitlines()
+        for e in self.events:
+            for l in out:
+                l = l.lstrip()
+                m = re.match("(.*)\s+.*\s+{}".format(e), l)
+                if m:
+                    value = m.group(1)
+                    self.timeseries.setdefault(e, []).append(float(value))
 
 profiling = Profiling()
-profiling.perf_power_events()
-#profiling.perf_stat_power()
-sys.exit(0)
 
 def profile_thread():
     while profiling.is_active:
-        print('perf')
-        profiling.perf_stat_power()
+        profiling.sample_perf_stat_power()
 
         profiling.terminate.acquire()
         profiling.terminate.wait(timeout=1)
@@ -43,6 +53,8 @@ def profile_thread():
 
 def start():
     global profiling
+    profiling.state_usage_start = power_state_usage()
+    profiling.state_time_start = power_state_time()        
     profiling.is_active=True
     x = threading.Thread(target=profile_thread)    
     x.daemon = True
@@ -55,12 +67,60 @@ def stop():
     profiling.terminate.notify()
     profiling.terminate.release()
 
+    profiling.state_usage_stop = power_state_usage()
+    profiling.state_time_stop = power_state_time()        
+
+def power_state_names():
+    state_names = []
+    stream = os.popen('ls /sys/devices/system/cpu/cpu0/cpuidle/')
+    states = stream.readlines()
+    for state in states:
+        state = state.strip()
+        stream = os.popen("cat /sys/devices/system/cpu/cpu0/cpuidle/{}/name".format(state))
+        state_names.append(stream.read().strip())
+    return state_names
+
+def power_state_metric(metric):
+    state_names = power_state_names()
+    cpu_state_values = []
+    for cpu in range(0, os.cpu_count()):
+        state_values = []
+        for state in range(0, len(state_names)):
+            output = open("/sys/devices/system/cpu/cpu{}/cpuidle/state{}/{}".format(cpu, state, metric)).read()
+            state_values.append(int(output))
+        cpu_state_values.append(state_values)
+    return cpu_state_values
+
+def power_state_usage():
+    return power_state_metric('usage')
+
+def power_state_time():
+    return power_state_metric('time')
+
+def power_state_diff(new_vector, old_vector):
+    diff = []
+    for (new, old) in zip(new_vector, old_vector):
+        diff.append([x[0] - x[1] for x in zip(new, old)])
+    return diff
+    
+def report():
+    global profiling
+    timeseries = profiling.timeseries
+    usage = []
+    usage.append(power_state_names())
+    usage.append(power_state_diff(profiling.state_usage_stop, profiling.state_usage_start))
+    time = []
+    time.append(power_state_names())
+    time.append(power_state_diff(profiling.state_time_stop, profiling.state_time_start))
+    return [timeseries, usage, time]
+
 def server(port):
     hostname = socket.gethostname().split('.')[0]
     server = SimpleXMLRPCServer((hostname, port), allow_none=True)
     print("Listening on port {}...".format(port))
     server.register_function(start, "start")
     server.register_function(stop, "stop")
+    server.register_function(report, "report")
     server.serve_forever()
 
 class StartAction:
@@ -85,6 +145,18 @@ class StopAction:
         with xmlrpc.client.ServerProxy("http://{}:{}/".format(args.hostname, args.port)) as proxy:
             proxy.stop()
 
+class ReportAction:
+    @staticmethod
+    def add_parser(subparsers):
+        parser = subparsers.add_parser('report', help = "Report profiling")
+        parser.set_defaults(func=ReportAction.action)
+
+    @staticmethod
+    def action(args):
+        with xmlrpc.client.ServerProxy("http://{}:{}/".format(args.hostname, args.port)) as proxy:
+            timeseries = proxy.report()
+            print(timeseries)
+
 def parse_args():
     """Configures and parses command-line arguments"""
     parser = argparse.ArgumentParser(
@@ -103,7 +175,7 @@ def parse_args():
         help="verbose")
 
     subparsers = parser.add_subparsers(dest='subparser_name', help='sub-command help')
-    actions = [StartAction, StopAction]
+    actions = [StartAction, StopAction, ReportAction]
     for a in actions:
       a.add_parser(subparsers)
 
